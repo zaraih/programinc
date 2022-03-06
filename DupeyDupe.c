f03d6bd20d9739cd10bc59306ffff382f5c9615a
zaraih <34193921+zaraih@users.noreply.github.com>
2022-03-05 19:57:24 -0600

    Add files via upload

---
+++ DupeyDupe.c
@@ -0,0 +1,78 @@
+//  Zarai Huete
+//  COP 3502, Spring 2019
+//  za780541
+
+#include "DupeyDupe.h"
+#include <stdio.h>
+#include <string.h>
+
+double difficultyRating(void);
+double hoursSpent(void);
+
+int main(int argc, char **argv)
+{
+  int i, j, duplicateFlag = 0, dupeFlag = 0;
+
+    for (i = (argc - 1); i > 0; i--)
+    {
+      if (duplicateFlag == 1)
+        break;
+      else
+      {
+        for (j = (i - 1); j > 0; j--)
+        {
+          if (duplicateFlag == 1)
+            break;
+          else
+          {
+            if (strcmp (argv[i], argv[j]) == 0)
+            {
+              printf("%s\n", argv[i]);
+              duplicateFlag = 1;
+            }
+          }
+        }
+      }
+    }
+
+    for (i = (argc - 1); i > 0; i--)
+    {
+      if (dupeFlag == 1)
+        break;
+      else
+      {
+        for (j = (i - 1); j > 0; j--)
+        {
+          if (dupeFlag == 1)
+            break;
+          else
+          {
+            if (strcmp(argv[i], "dupe") == 0 && strcmp(argv[j], "dupe") == 0)
+            {
+              if (i - 1 == j)
+              {
+                printf("dupe dupe!\n");
+                dupeFlag = 1;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    if (duplicateFlag == 0)
+       printf("no dupey dupe :(\n");
+
+return 0;
+}
+
+
+double difficultyRating(void)
+{
+  return 3.0;
+}
+
+double hoursSpent(void)
+{
+  return 4.5;
+}
